6761b411ad1d8e6519cc63a500f163696d86630c
Guilherme Guimaraes <guilhermeguimaraes970@gmail.com>
2025-01-28 21:12:26 -0300

    Associação unidirecional muitos para um

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTeste02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Time;
+
+public class JogadorTeste02 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Ronaldinho Gaúcho");
+        Time time = new Time("Seleção brasileira");
+
+        jogador1.setTime(time);
+        jogador1.imprimir();
+
+
+    }
+}
